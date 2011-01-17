79b609ea17d4720a0ec44f10b24b94cf430158a5
Marc-Oliver Teschke <marc@Marc-Oliver-Teschkes-MacBook.local>
2011-01-17 07:56:01 +0100

    Added readme.md, added e-mail address.

---
+++ README.md
@@ -0,0 +1,7 @@
+# 30daylist.com
+
+The website for 30daylist, a magical and reeevolutionary new iPhone app.
+
+## Uses…
+
+* html5boilerplate (http://html5boilerplate.com/)
--- htdocs/js/script.js
+++ htdocs/js/script.js
@@ -2,7 +2,14 @@
 
 */
 
+jQuery.fn.mailto = function() {
+	return this.each(function(){
+		var email = $(this).html().replace(/\s*\(.+\)\s*/, "@");
+		$(this).before('<a href="mailto:' + email + '" rel="nofollow" title="Email ' + email + '">' + email + '</a>').remove();
+	});
+};
 
+$('.mailto').mailto();
 
 
 
